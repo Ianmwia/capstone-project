58c8e7e9b753fcefaaed5197e578bfa73b233275
Mwia <iansnr10@gmail.com>
2025-10-17 09:24:44 +0300

    adding a function to update the location of iss after some time, rounded of units, unicode and clear appending data on html

--- iss/iss.js
+++ iss/iss.js
@@ -1,16 +1,20 @@
 //where is the iss -- free
 const issApi = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-fetch(issApi)
-.then(response => response.json())
-.then(data => {
-    console.log('iss api is working')
-    console.log(data)
-    displayIssData(data)
-})
-.catch(error =>{
-    console.error('iss api not working', error)
-})
+function fetchDataInIntervals(){
+    fetch(issApi)
+    .then(response => response.json())
+    .then(data => {
+        console.log('iss api is working')
+        console.log(data)
+        displayIssData(data)
+    })
+    .catch(error =>{
+        console.error('iss api not working', error)
+    })
+}
+fetchDataInIntervals()
+setInterval(fetchDataInIntervals, 3000)
 
 //dom
 const issLocation = document.getElementById('location')
@@ -26,8 +30,13 @@ function appendParagraph(containerId, text){
 
 //display data
 function displayIssData(data){
-    appendParagraph(issLocation,`Latitude:${data.latitude}`)
-    appendParagraph(issLocation,`Longitude:${data.longitude}`)
-    appendParagraph(issAltitude,`Altitude:${data.altitude} km`)
-    appendParagraph(issSpeed,`Orbital Speed:${data.velocity} km/h`)
+    //clear appending data after set interval
+    issLocation.innerHTML = ''
+    issAltitude.innerHTML = ''
+    issSpeed.innerHTML = ''
+
+    appendParagraph(issLocation,`Latitude:${data.latitude.toFixed(3)}\u00B0`)
+    appendParagraph(issLocation,`Longitude:${data.longitude.toFixed(3)}\u00B0`)
+    appendParagraph(issAltitude,`Altitude:${data.altitude.toFixed(3)} km`)
+    appendParagraph(issSpeed,`Orbital Speed:${data.velocity.toFixed(3)} km/h`)
 }
